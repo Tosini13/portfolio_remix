5781992d385c54c2e256b72b6f518577ba1b018d
Tosini13 <jbartos13@gmail.com>
2022-12-30 10:46:36 +0100

    added prisma db

---
+++ prisma/seed.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
+
+async function seed() {
+  await Promise.all(
+    getSocialMedia().map((joke) => {
+      return db.socialMedia.create({ data: joke });
+    })
+  );
+}
+
+seed();
+
+function getSocialMedia() {
+  return [
+    {
+      id: "github",
+      name: "github",
+      icon: `github.svg`,
+      url: "#",
+    },
+    {
+      id: "linkedIn",
+      name: "linkedIn",
+      icon: `linkedIn.svg`,
+      url: "#",
+    },
+    {
+      id: "fb",
+      name: "fb",
+      icon: `facebook.svg`,
+      url: "#",
+    },
+    {
+      id: "insta",
+      name: "insta",
+      icon: `instagram.svg`,
+      url: "#",
+    },
+    {
+      id: "mail",
+      name: "mail",
+      icon: `mail.secondary.icon.svg`,
+      url: "#",
+    },
+    {
+      id: "phone",
+      name: "phone",
+      icon: `telephone.secondary.icon.svg`,
+      url: "#",
+    },
+  ];
+}
